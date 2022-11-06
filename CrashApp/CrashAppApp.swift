65294486ab2d0e6414e9611e5a4196570c683947
Saydullo <sadi.abdulazizov.2001@gmail.com>
2022-11-06 10:25:31 +0500

    Initial Commit

---
+++ CrashApp/CrashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashAppApp.swift
+//  CrashApp
+//
+//  Created by Saydullo on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CrashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
